66cb26a79c423cce945736b44391cbb3bb0a9821
fredrik-sundstrom-nti-johanneberg <fredrik.sundstrom@elev.ga.ntig.se>
2021-03-12 14:36:10 +0100

    Update app.rb

--- app.rb
+++ app.rb
@@ -14,22 +14,22 @@ get('/showlogin') do
     slim(:login)
 end 
 
-get('/annons') do 
+get('/annons/new') do 
     slim(:annons)
 end 
 
 post('/login') do
     username = params[:username]
     password = params[:password]
-    db = SQLite3::Database.new('db/todo2021.db')
+    db = SQLite3::Database.new('db/databas.db')
     db.results_as_hash = true
-    result = db.execute("SELECT *FROM users WHERE username = ?",username).first
+    result = db.execute("SELECT *FROM user WHERE username = ?",username).first
     pwdigest = result["pwdigest"]
     id = result["id"]
   
     if BCrypt::Password.new(pwdigest) == password
       session[:id] = id
-      redirect('/todos')
+      redirect('/annons/new')
     else
       "Fel lösenord!"
     end
@@ -42,8 +42,8 @@ post('/users/new') do
   
     if (password == password_confirm)
       password_digest = BCrypt::Password.create(password)
-      db = SQLite3::Database.new('db/todo2021.db')
-      db.execute("INSERT INTO users (username,pwdigest) VALUES (?,?)",username,password_digest)
+      db = SQLite3::Database.new('db/databas.db')
+      db.execute("INSERT INTO user (username,pwdigest) VALUES (?,?)",username,password_digest)
       redirect('/')
     else
       "Lösenorden matchade inte!"
@@ -57,5 +57,5 @@ post('/annons/new') do
 
     db = SQLite3::Database.new('db/databas.db')
     db.execute("INSERT INTO annons (rubrik,bio,pris) VALUES (?,?,?)",rubrik,bio,pris)
-    redirect('/index')
+    redirect('/')
 end
